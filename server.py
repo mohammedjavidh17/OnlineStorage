3640eb28cd2cdb92ea1cb779933475d9a4ece2d6
Javidh <98268633+mohammedjavidh17@users.noreply.github.com>
2022-09-04 09:23:00 +0530

    Update server.py

--- server.py
+++ server.py
@@ -9,7 +9,6 @@ print ("Server running", HOST, PORT)
 s.listen(1)
 cnt =0
 
-
 #predefines
 def reply(clnt, rep):
     ts = 'READY'
@@ -41,6 +40,10 @@ while True:
     while True:
         rep = conn.recv(1024)
         print(rep)
+        try:
+            rep.split()[0]
+        except:
+            continue
         if rep.split()[0].decode('utf-8') == 'AUTH':
             if Authn(rep):
                 conn.send(b'GET')
@@ -48,9 +51,48 @@ while True:
                 #assets\data\U10001\config.csv
                 file = "assets\data\\"+'U'+str(UserId.decode('utf-8'))+"\config.csv"
                 f = open(file)
-                for row in f:
-                    conn.send(row.encode('utf-8'))
-                conn.send("READY")
-                cmd2 = conn.recv(1024)
-            else:
+                conn.recv(1)
+                conn.sendall(f.read().encode('utf-8'))  #SEND FILE
+                f.close()
+                conn.recv(1)
+                while True:
+                    conn.send(b"READY")
+                    cmd2 = conn.recv(1024)
+                    try:
+                        cmd2.split()[0]
+                    except:
+                        continue
+                    if cmd2.split()[0] == b'READ':
+                        lstCmd2 = cmd2.split()   #['READ', '<FILENO>/INDX']
+                        file = "assets\data\\"+'U'+str(UserId.decode('utf-8'))+"\\"+str(lstCmd2[1].decode('utf-8'))+".txt"
+                        f = open(file)
+                        conn.sendall(f.read().encode('utf-8')) #SEND FILE
+                        f.close()
+                        conn.recv(1)
+                        conn.send(b"READY")
+                    elif cmd2.split()[0] == b'WRITE':  #['WRITE', '<FileNAme>', extention]
+                        lstCmd2 = cmd2.split()
+                        config = pd.read_csv(file)
+                        configN = pd.DataFrame({
+                            "Fileno" : [config.shape[0]+1],
+                            "FileName": [lstCmd2[1].decode('utf-8')],
+                            "extention":[lstCmd2[2].decode('utf-8')]
+                        })
+                        config = config.append(configN, ignore_index=True)
+                        conn.send(b'READY')
+                        fileTXT = conn.recv(40000000)
+                        file = "assets\data\\"+'U'+str(UserId.decode('utf-8'))+"\\"+str(config.shape[0])+".txt"
+                        DES = open(file, 'w')
+                        DES.write(str(fileTXT.decode('utf-8')))
+                        file = "assets\data\\"+'U'+str(UserId.decode('utf-8'))+"\config.csv"
+                        config.to_csv(file, index=False)
+                    elif cmd2.split()[0] == "NEW": #[NEW, "ID", "pas"]
+                        NewId = cmd2.split()[1]
+                        file = "assets\data\\U"+str(NewId)+"\\config.csv"
+                        Sam = pd.read_csv("EmpytyConfig.csv")
+                        Sam.to_csv(file, index=False)
+                        conn.send(b'DONE')  #Confirmation
+                    elif cmd2.split()[0] == 'DIS': #["DIS", "DUMMY"]
+                        conn.close()
+            else: 
                 conn.send(b"FAILED")
